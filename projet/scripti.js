659ae83a74a32ee20201c43ddba0299e3f7bdc37
AbderrahmaneSajid <117985570+AbderrahmaneSajid@users.noreply.github.com>
2022-12-23 11:34:54 +0100

    Update scripti.js

--- projet/scripti.js
+++ projet/scripti.js
@@ -72,3 +72,58 @@ img3.onmouseout=function(){
     info3.style.display =  "none";
     
 }
+let img4=document.getElementById("img4");
+let info4=document.getElementById("info4");
+info4.style.display =  "none";
+img4.onmouseover=function(){
+    info4.style.display =  "block";
+    info4.style.visibility= "visible";
+}
+img4.onmouseout=function(){
+    info4.style.display =  "none";
+    
+}
+let img5=document.getElementById("img5");
+let info5=document.getElementById("info5");
+info5.style.display =  "none";
+img5.onmouseover=function(){
+    info5.style.display =  "block";
+    info5.style.visibility= "visible";
+}
+img5.onmouseout=function(){
+    info5.style.display =  "none";
+    
+}
+let img6=document.getElementById("img6");
+let info6=document.getElementById("info6");
+info6.style.display =  "none";
+img6.onmouseover=function(){
+    info6.style.display =  "block";
+    info6.style.visibility= "visible";
+}
+img6.onmouseout=function(){
+    info6.style.display =  "none";
+    
+}
+let img7=document.getElementById("img7");
+let info7=document.getElementById("info7");
+info7.style.display =  "none";
+img7.onmouseover=function(){
+    info7.style.display =  "block";
+    info7.style.visibility= "visible";
+}
+img7.onmouseout=function(){
+    info7.style.display =  "none";
+    
+}
+let img8=document.getElementById("img8");
+let info8=document.getElementById("info8");
+info8.style.display =  "none";
+img8.onmouseover=function(){
+    info8.style.display =  "block";
+    info8.style.visibility= "visible";
+}
+img8.onmouseout=function(){
+    info8.style.display =  "none";
+    
+}
