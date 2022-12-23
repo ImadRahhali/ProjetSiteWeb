5fb72845e74ce593cf6c041531af304d3375df36
AbderrahmaneSajid <117985570+AbderrahmaneSajid@users.noreply.github.com>
2022-12-23 10:30:23 +0100

    Update scripti.js

--- projet/scripti.js
+++ projet/scripti.js
@@ -6,3 +6,69 @@ window.onscroll = () =>{
       }
     
     }
+let img=document.getElementById("img");
+let info=document.getElementById("info");
+info.style.display =  "none";
+img.onmouseover=function(){
+    info.style.display =  "block";
+    info.style.visibility= "visible";
+}
+img.onmouseout=function(){
+    info.style.display =  "none";
+    
+}
+let imgg=document.getElementById("imgg");
+let infoo=document.getElementById("infoo");
+infoo.style.display =  "none";
+imgg.onmouseover=function(){
+    infoo.style.display =  "block";
+    infoo.style.visibility= "visible";
+}
+imgg.onmouseout=function(){
+    infoo.style.display =  "none";
+    
+}
+let imggg=document.getElementById("imggg");
+let infooo=document.getElementById("infooo");
+infooo.style.display =  "none";
+imggg.onmouseover=function(){
+    infooo.style.display =  "block";
+    infooo.style.visibility= "visible";
+}
+imggg.onmouseout=function(){
+    infooo.style.display =  "none";
+    
+}
+let img1=document.getElementById("img1");
+let info1=document.getElementById("info1");
+info1.style.display =  "none";
+img1.onmouseover=function(){
+    info1.style.display =  "block";
+    info1.style.visibility= "visible";
+}
+img1.onmouseout=function(){
+    info1.style.display =  "none";
+    
+}
+let img2=document.getElementById("img2");
+let info2=document.getElementById("info2");
+info2.style.display =  "none";
+img2.onmouseover=function(){
+    info2.style.display =  "block";
+    info2.style.visibility= "visible";
+}
+img2.onmouseout=function(){
+    info2.style.display =  "none";
+    
+}
+let img3=document.getElementById("img3");
+let info3=document.getElementById("info3");
+info3.style.display =  "none";
+img3.onmouseover=function(){
+    info3.style.display =  "block";
+    info3.style.visibility= "visible";
+}
+img3.onmouseout=function(){
+    info3.style.display =  "none";
+    
+}
